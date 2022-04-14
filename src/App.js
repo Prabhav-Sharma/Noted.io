2a365ea9ef5bcf2cb8414df2488809d56f352084
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-04-14 17:31:17 +0530

    Feat: Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Router from "./router/Router";
 
 function App() {
-  return <div className="App">Noted.io</div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Router />
+    </div>
+  );
 }
 
 export default App;
