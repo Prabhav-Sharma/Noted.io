966346880d65cb3663072c75cf7987069e676d7c
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-04-30 15:55:22 +0530

    Feat: Add UnArchive icon

--- src/icons.js
+++ src/icons.js
@@ -1,6 +1,6 @@
 import { FaHome, FaTrash, FaSave } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
-import { MdLabel, MdArchive } from "react-icons/md";
+import { MdLabel, MdArchive, MdUnarchive } from "react-icons/md";
 import { IoMdColorPalette, IoMdTrash } from "react-icons/io";
 import { BsPinFill } from "react-icons/bs";
 import { GiNotebook } from "react-icons/gi";
@@ -23,4 +23,5 @@ export {
   AiOutlineEyeInvisible,
   GrClose,
   RiLoaderFill,
+  MdUnarchive,
 };
