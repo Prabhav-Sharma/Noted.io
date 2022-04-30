5f6f580c481f64cda019cd3d983914813ec29b20
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-04-30 15:59:42 +0530

    Feat: Add helper functions

---
+++ src/components/helpers.js
@@ -0,0 +1,60 @@
+const noteReducer = (state, action) => {
+  let updatedLabels;
+  switch (action.type) {
+    case "ADD_LABEL":
+      updatedLabels = [...state.note.labels, action.payload.label];
+      return {
+        ...state,
+        note: { ...state.note, labels: updatedLabels },
+      };
+
+    case "REMOVE_LABEL":
+      updatedLabels = state.note.labels.filter(
+        (label) => label.text !== action.payload.label.text
+      );
+      return { ...state, note: { ...state.note, labels: updatedLabels } };
+
+    case "NOTE_COLOR":
+      return {
+        ...state,
+        note: { ...state.note, noteColor: action.payload.noteColor },
+      };
+
+    case "LABEL_TOGGLE":
+      return { ...state, labelToggle: !state.labelToggle };
+
+    case "SAVE_TOGGLE":
+      return { ...state, saveToggle: action.payload.saveToggle };
+
+    case "SAVE_LOADING":
+      return { ...state, saveLoading: !state.saveLoading };
+
+    case "PINNED":
+      return { ...state, note: { ...state.note, pinned: !state.note.pinned } };
+
+    default:
+      return state;
+  }
+};
+
+function isBright(hex) {
+  const red = parseInt(hex[1] + hex[2], 16);
+  const green = parseInt(hex[3] + hex[4], 16);
+  const blue = parseInt(hex[5] + hex[6], 16);
+  const luminance = 0.2126 * red + 0.7152 * green + 0.0722 * blue; // taken and understood from stackoverflow
+  return luminance > 50 ? true : false;
+}
+
+const labelColors = [
+  "#E74B7F",
+  "#89E6D8",
+  "#32C356",
+  "#E3D8F1",
+  "#FDE74C",
+  "#058ED9",
+];
+
+const getRandomColor = () =>
+  labelColors[Math.floor(Math.random() * labelColors.length)];
+
+export { noteReducer, isBright, getRandomColor };
