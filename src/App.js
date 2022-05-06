ca6a27b03c825bcc8d4aa93464f83442935c018f
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-06 17:33:36 +0530

    Feat: Add container & Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
-import { TopBar, LogoutButton } from "./components";
+import { TopBar } from "./components";
 import Router from "./router/Router";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={800}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+        theme="colored"
+        limit={2}
+      />
       <TopBar />
       <Router />
     </div>
