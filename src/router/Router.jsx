034fa14240d92f55a5bbf13a54ca74610aa60d9c
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-20 23:24:44 +0530

    Feat: Add routes for pages

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { MockAPI, Landing, Home } from "../pages";
+import { Layout } from "../components";
+import {
+  MockAPI,
+  Landing,
+  Home,
+  Profile,
+  Search,
+  Trash,
+  Archives,
+} from "../pages";
 import Note from "../pages/Note";
 import ProtectedRoutes from "./ProtectedRoutes";
 
@@ -10,7 +19,47 @@ function Router() {
       <Route path="/" element={<Landing />}></Route>
       <Route path="/mockman" element={<MockAPI />}></Route>
       <Route element={<ProtectedRoutes />}>
-        <Route path="/home" element={<Home />}></Route>
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/trash"
+          element={
+            <Layout>
+              <Trash />
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <Search />
+            </Layout>
+          }
+        />
+        <Route
+          path="/archives"
+          element={
+            <Layout>
+              <Archives />
+            </Layout>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <Profile />
+            </Layout>
+          }
+        />
+
         <Route path="/note/:type/:noteId" element={<Note />} />
       </Route>
     </Routes>
